98ed0d3e1f99a4467531447d9509b8e3bdac8342
Abid Hasan <144882936+abid450@users.noreply.github.com>
2023-09-14 11:23:17 +0600

    I have upload htnl, css and jquery file

---
+++ index.js
@@ -0,0 +1,14 @@
+
+/*$("#btn").click(function(){
+    $("#div1").slideToggle(3000);
+})
+$("#btn").click(function(){
+    $("#div1").slideToggle(3000);
+})*/
+
+$("#btn").click(function(){
+    $("#div1").fadeToggle(2000);
+})
+$("#btn").click(function(){
+    $("#div1").slideToggle(2000);
+})
